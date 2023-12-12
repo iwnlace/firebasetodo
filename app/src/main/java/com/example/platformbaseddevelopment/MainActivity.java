7d7c70f3fa5321b64cde52fd8e30e7e91259a968
iwnlace <aceespinosaa@gmail.com>
2023-12-12 22:10:17 +0800

    commit

---
+++ app/src/main/java/com/example/platformbaseddevelopment/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.platformbaseddevelopment;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
+    private FrameLayout frameLayout;
+    FirebaseAuth auth;
+    Button button;
+    TextView textView;
+    FirebaseUser user;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottomView);
+        frameLayout = findViewById(R.id.frameLayout);
+
+        auth = FirebaseAuth.getInstance();
+        button = findViewById(R.id.logout);
+        textView = findViewById(R.id.user_details);
+        user = auth.getCurrentUser();
+        if (user == null) {
+            Intent intent = new Intent(getApplicationContext(), Login.class);
+            startActivity(intent);
+            finish();
+        }
+        else {
+            textView.setText(user.getEmail());
+        }
+
+        button.setOnClickListener(v -> {
+            FirebaseAuth.getInstance().signOut();
+            Intent intent = new Intent(getApplicationContext(), Login.class);
+            startActivity(intent);
+            finish();
+        });
+    }
+}
---
+++ build.gradle.kts
@@ -0,0 +1,16 @@
+buildscript {
+    val kotlin_version by extra("2.0.0-Beta1")
+    dependencies {
+        classpath("com.google.gms:google-services:4.4.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version")
+    }
+    repositories {
+        mavenCentral()
+    }
+}
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    id("com.android.application") version "8.1.4" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.0-Beta1" apply false
+}
+
